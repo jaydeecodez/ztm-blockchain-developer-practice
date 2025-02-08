501af6e86ecd9c952471b74e04e1977224ed204f
Jacob Dowdy <jacobdowdy@rocketmail.com>
2025-02-08 01:35:01 -0500

    add override and virtual keyword examples

--- parent-child-ex.sol
+++ parent-child-ex.sol
@@ -2,23 +2,29 @@
 pragma solidity 0.8.26;
 
 contract Father {
-    uint256 public myNumber1;
+    uint256 private myNumber;
 
-    function setMyNumber1(uint256 newNumber) external  {
-        myNumber1 = newNumber;
+    function setMyNumber(uint256 newNumber) external virtual {
+        myNumber = newNumber;
     }
 }
 
-contract Child1 is Father {
-    uint256 public myNumber2;
+contract Mother {
+    uint256 private myNumber;
 
-    function setMyNumber2(uint256 newNumber) external  {
-        myNumber2 = newNumber;
+    function setMyNumber(uint256 newNumber) external virtual {
+        myNumber = newNumber;
     }
 }
 
-contract Child2 is Father {
-    function addToBothNumbers(uint256 addition) external  {
-        myNumber1 += addition;
+contract Child is Mother, Father {
+    uint256 private myNumber;
+    
+    function addToMyNumber(uint256 addition) external {
+        myNumber += addition;
+    }
+
+    function setMyNumber(uint256 newNumber) external override(Mother, Father) {
+        myNumber = 5;
     }
 }
