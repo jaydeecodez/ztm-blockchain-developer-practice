68b681399f1984b34cbd49ac1d511d35ef31fc95
Jacob Dowdy <jacobdowdy@rocketmail.com>
2025-02-10 12:42:07 -0500

    add BaseSetUp contract

--- test/ERC20.t.sol
+++ test/ERC20.t.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {ERC20} from "../src/ERC20.sol";
 
-contract ERC20Test is ERC20, Test {
-    address private alice;
-    address private bob;
-    
+contract BaseSetup is ERC20, Test {
+    address internal alice;
+    address internal bob;
+
     constructor() ERC20("name", "SYM", 18) {}
 
     function setUp() public virtual {
@@ -16,13 +16,21 @@ contract ERC20Test is ERC20, Test {
 
         _mint(alice, 300e18);
     }
+}
+
+contract ERC20TransferTest is BaseSetup {
+    function setUp() public override {
+        BaseSetup.setUp();
+    }
 
     function testTransfersTokenCorrectly() public {
         vm.prank(alice);
         bool success = this.transfer(bob, 100e18);
         assertTrue(success);
 
-        assertEqDecimal(this.balanceOf(alice), 100e18, decimals);
+        assertEqDecimal(this.balanceOf(alice), 200e18, decimals);
         assertEqDecimal(this.balanceOf(bob), 100e18, decimals);
     }
 }
+
+contract ERC20TransferFromTest is BaseSetup {}
