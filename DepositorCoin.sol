7741ef481ea49ce41b69152682ae080e08be00c5
Jacob Dowdy <jacobdowdy@rocketmail.com>
2025-02-22 08:13:50 -0500

    Create DepositorCoin.sol

---
+++ DepositorCoin.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {ERC20} from "./ERC20.sol";
+
+contract DepositorCoin is ERC20 {
+    address public owner;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 value) external {
+        require(msg.sender = owner, "DPC: Only owner can mint");
+
+        _mint(to, value);
+    }
+
+    function burn(address from, uint256 value) external {
+        require(msg.sender = owner, "DPC: Only owner can mint");
+        _burn(from, value);
+    }
+}
