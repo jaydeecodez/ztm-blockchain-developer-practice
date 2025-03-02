d93097b8eb5e74efa2cf23fe4ae26bbf5e10bb3b
Jacob Dowdy <jacobdowdy@rocketmail.com>
2025-03-02 10:51:31 -0500

    add timelocked depositor coin functionality

--- Stablecoin.sol
+++ Stablecoin.sol
@@ -10,17 +10,20 @@ contract StableCoin is ERC20 {
     Oracle public oracle;
     uint256 public feeRatePercentage;
     uint256 public initialCollateralRatioPercentage;
+    uint256 public depositorCoinLockTime;
 
     constructor(
         string memory _name,
         string memory _symbol,
         Oracle _oracle,
         uint256 _feeRatePercentage,
-        uint256 _initialCollateralRatioPercentage
+        uint256 _initialCollateralRatioPercentage,
+        uint256 _depositorCoinLockTime
     ) ERC20(_name, _symbol, 18) {
         oracle = _oracle;
         feeRatePercentage = _feeRatePercentage;
         initialCollateralRatioPercentage = _initialCollateralRatioPercentage;
+        depositorCoinLockTime = _depositorCoinLockTime;
     }
 
     function mint() external payable {
@@ -66,7 +69,11 @@ contract StableCoin is ERC20 {
                 "STC: Initial collateral ratio not met"
             );
 
-            depositorCoin = new DepositorCoin("Depositor Coin", "DPC");
+            depositorCoin = new DepositorCoin(
+                "Depositor Coin",
+                "DPC",
+                depositorCoinLockTime
+            );
             // new surplus: (msg.value - deficitInEth) * oracle.getPrice();
 
             usdInDpcPrice = 1;
